8d6561ed4dee4d5f90e015ac60b5d336c5c9aeaf
Yalanday <oeb72umvd@mial.ru>
2023-10-07 04:54:24 +0300

    Вторая функция ДЗ №2. 'Палиндром'

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,23 @@
 
 // 1
-const checksLenghtStr = (string, maxLengthStr) => string.length <= maxLengthStr;
+{
+  const checksLenghtStr = (string, maxLengthStr) => string.length <= maxLengthStr;
 
-console.log(checksLenghtStr('проверяемая строка', 10));
+  console.log(checksLenghtStr('проверяемая строка', 10));
+}
+
+// 2
+{
+  const polydromeCheck = function(string) {
+    const normalizeString = string.replaceAll(' ', '').toLowerCase();
+    let newString = '';
+
+    for (let i = normalizeString.length - 1; i >= 0; i--) {
+      newString += normalizeString[i];
+    }
+
+    return normalizeString === newString ? console.log(true) : console.log(false);
+  };
+  polydromeCheck('Лёша на полке клопа нашёл ');
+}
 
