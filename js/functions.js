fe0d8d4e3588b08d929b279c013ad55261a5f2f8
Yalanday <oeb72umvd@mial.ru>
2023-10-08 18:23:14 +0300

    Исправление ошибок, указанных наставником

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,23 @@
-/*      1      */
-{
-  const checksLenghtStr = (string, maxLengthStr) => string.length <= maxLengthStr;
+const checkStringLength = (string, maxLengthStr) => string.length <= maxLengthStr;
 
-  checksLenghtStr('проверяемая строка', 10);
-}
+checkStringLength('проверяемая строка', 10);
 
-/*      2      */
-{
-  const polydromeCheck = function (string) {
-    const normalizeString = string.replaceAll(' ', '').toLowerCase();
-    let newString = '';
+const palindromeCheck = (string) => {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  let newString = '';
 
-    for (let i = normalizeString.length - 1; i >= 0; i--) {
-      newString += normalizeString[i];
-    }
+  for (let i = normalizeString.length - 1; i >= 0; i--) {
+    newString += normalizeString[i];
+  }
 
-    return normalizeString === newString;
-  };
-  polydromeCheck('Лёша на полке клопа нашёл ');
-}
+  return normalizeString === newString;
+};
 
-/*      3      */
-{
-  const extractNum = function (string) {
-    const valum = string.toString().replace(/\D/g, '');
-    return (parseInt(valum, 10));
-  };
+palindromeCheck('Лёша на полке клопа нашёл ');
 
-  extractNum('1 кефир, 0.5 батона');
-}
+const extractNum = (string) => {
+  const result = string.toString().replace(/\D/g, '');
+  return (parseInt(result, 10));
+};
 
+extractNum('1 кефир, 0.5 батона');
