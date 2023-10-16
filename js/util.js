b2a332866a73ad5eda8c5d7f575a4b979ee4142e
Yalanday <oeb72umvd@mial.ru>
2023-10-16 22:18:52 +0300

    Разделяет main.js на модули. Добавляет модули data.js и util.js

---
+++ js/util.js
@@ -0,0 +1,22 @@
+const getRandomIntegrated = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.ceil(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getUniqueIdElement = (min, max) => {
+  const createUniqueIdArray = [];
+  return () => {
+    let createUnicumCurrentId = getRandomIntegrated(min, max);
+    while (createUniqueIdArray.includes(createUnicumCurrentId)) {
+      createUnicumCurrentId = getRandomIntegrated(min, max);
+    }
+    createUniqueIdArray.push(createUnicumCurrentId);
+    return createUniqueIdArray;
+  };
+};
+
+export {getRandomIntegrated};
+export {getUniqueIdElement};
