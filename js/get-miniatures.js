ca97542e2b1489c3abc065a56e33fb498a5747be
Yalanday <oeb72umvd@mial.ru>
2023-10-19 00:43:48 +0300

    ДЗ-7.15: Добавляет модуль с кодом создания DOM элементов - фотоМиниатюр

---
+++ js/get-miniatures.js
@@ -0,0 +1,21 @@
+import { createArrayPhoto } from './data.js';
+
+const miniaturesList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content;
+
+const createPictures = createArrayPhoto;
+
+const createListPictures = document.createDocumentFragment();
+
+createPictures.forEach(({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  createListPictures.appendChild(pictureElement);
+});
+
+miniaturesList.appendChild(createListPictures);
+
+export { miniaturesList };
