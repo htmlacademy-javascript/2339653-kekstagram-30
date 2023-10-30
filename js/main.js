302c9e34ad0e33d069d6321515cf9f8a6512b8df
Yalanday <oeb72umvd@mial.ru>
2023-10-30 21:06:41 +0300

    Создал ветку и файл-модуль для ДЗ № 11

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { openUploadPictureModal } from './form-modal-window.js';
 import { getPictures } from './modal-big-picture.js';
 import { checksFormValidation } from './validation-data.js';
+import './server.js';
 
 getPictures();
 openUploadPictureModal();
