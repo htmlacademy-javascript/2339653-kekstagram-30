789ff0176803968acd19a3ae5670abba5566487d
Yalanday <oeb72umvd@mial.ru>
2023-11-01 04:02:50 +0300

    Удалил файл data.js и лишние функции из util.js

--- js/util.js
+++ js/util.js
@@ -1,25 +1,5 @@
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-const getRandomIntegrated = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.ceil(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const getUniqueIdElement = (min, max) => {
-  const createUniqueIdArray = [];
-  return () => {
-    let createUnicumCurrentId = getRandomIntegrated(min, max);
-    while (createUniqueIdArray.includes(createUnicumCurrentId)) {
-      createUnicumCurrentId = getRandomIntegrated(min, max);
-    }
-    createUniqueIdArray.push(createUnicumCurrentId);
-    return createUniqueIdArray;
-  };
-};
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const stopIsEscapeKey = (element) => {
@@ -50,8 +30,6 @@ const debounce = (callback, timeoutDelay) => {
 };
 
 export { checkStringLength };
-export { getRandomIntegrated };
-export { getUniqueIdElement };
 export { isEscapeKey };
 export { stopIsEscapeKey };
 export { onModalEscapeKeydown };
